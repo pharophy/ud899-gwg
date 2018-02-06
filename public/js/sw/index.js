1428dec1e079a93cb3f8a9b5493ef7faa300c38b
SHAWN-LAPTOP\Shawn Souto <shawn.souto@gmail.com>
2018-02-05 21:47:39 -0500

    handle responses for local cached items (cache-served)

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,35 +1,37 @@
+import 'babel-polyfill';
+
 self.addEventListener('install', (event) => {
-  var urls = [
+
+  const cacheIndex =  [
     '/',
     '/js/main.js',
     'css/main.css',
     'imgs/icon.png',
     'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
     'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-  ];
-  
+];
+
   event.waitUntil(
     caches.open('wittr-static-v1').then((cache) => {
-      cache.addAll(urls);
+      cache.addAll(cacheIndex);
     })
   );
 
 });
 
-self.addEventListener('fetch', function(event) {
+const cachedResource = async (request) => {
+  const cache = await caches.open('wittr-static-v1')
+  let response = await cache.match(request);
+  if (response) {
+    return response;
+  } else {
+    return await fetch(request);
+  }
+}
+
+self.addEventListener('fetch', (event) => {
   
   console.log(event);
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      if (response.status === 404) {
-        console.log(response, ' failed');
-        return fetch('/imgs/dr-evil.gif');
-      }
-      return response;
-    }).catch((error) => {
-      console.log(error);
-      return new Response("Uh oh, something's wrong.");
-    })
-  );
+  event.respondWith(cachedResource(event.request));
   
 });
