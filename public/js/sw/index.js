a7a645fe4a4e769fba42293c764a0af52f1122cf
SHAWN-LAPTOP\Shawn Souto <shawn.souto@gmail.com>
2018-02-05 20:33:12 -0500

    code for challenge install-cached

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,21 @@
+self.addEventListener('install', (event) => {
+  var urls = [
+    '/',
+    '/js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+  ];
+  
+  event.waitUntil(
+    caches.open('wittr-static-v1').then((cache) => {
+      cache.addAll(urls);
+    })
+  );
+
+});
+
 self.addEventListener('fetch', function(event) {
   
   console.log(event);
